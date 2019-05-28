cf7a8f3b516d1a06287ff0274f461327912b31e8
preil <preil2000@gmail.com>
2019-05-28 08:10:30 +0300

    Editing meetup dialog for title and description finished.

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -1,13 +1,22 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout row wrap v-if="loading">
+      <v-flex xs12 text-xs-center>
+        <v-progress-circular
+          indeterminate
+          class="primary--text"
+          :width="7"
+          :size="70"></v-progress-circular>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap v-else>
       <v-flex xs12>
         <v-card>
           <v-card-title>
             <h2 class="primary--text">{{ meetup.title }}</h2>
             <template v-if="userIsCreator">
               <v-spacer></v-spacer>
-              <app-edit-meetup-details-dialog></app-edit-meetup-details-dialog>
+              <app-edit-meetup-details-dialog :meetup="meetup"></app-edit-meetup-details-dialog>
             </template>
           </v-card-title>
           <v-card-media
@@ -42,6 +51,9 @@
       userIsCreator() {
         if (!this.userIsAuthenticated) return false
         return this.$store.getters.user.id === this.meetup.creatorId
+      },
+      loading(){
+        return this.$store.getters.loading
       }
     }
   }
